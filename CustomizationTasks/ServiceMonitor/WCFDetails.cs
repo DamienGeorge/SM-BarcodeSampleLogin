3a81a1f40c18d205883ada9e3fcb2dfad94cfbac
Dan Eipe George <daneipegeorge@gmail.com>
2025-05-06 11:06:45 +0100

    Service Monitor

---
+++ CustomizationTasks/ServiceMonitor/WCFDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace Customization.Tasks
+{
+    public class WCFDetail
+    {
+        public WCFDetail(string url, DateTime lastCheckIn, bool isResponsive)
+        {
+            this.url = url;
+            LastCheckIn = lastCheckIn;
+            IsResponsive = isResponsive;
+        }
+
+        public WCFDetail()
+        {
+            
+        }
+
+        public string url { get; set; }
+        public DateTime LastCheckIn { get; set; }
+        public bool IsResponsive { get; set; }
+    }
+}
+
