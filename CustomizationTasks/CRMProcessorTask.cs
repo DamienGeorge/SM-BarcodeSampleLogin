18f09a21f749ebf9a90c35cc263bd22f9d24f3ab
DamienGeorge <daneipegeorge@gmail.com>
2025-09-05 15:40:55 +0100

    rebase

---
+++ CustomizationTasks/CRMProcessorTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thermo.SampleManager.Common.CommandLine;
+using Thermo.SampleManager.Library;
+using Thermo.SampleManager.ObjectModel;
+
+namespace Customization.Tasks
+{
+    public class CRMProcessorTask : SampleManagerTask, IBackgroundTask
+    {
+        protected override void SetupTask()
+        {
+            if (Context.SelectedItems.Count == 0)
+            {
+                return;
+            }
+
+            Workflow workflow = 
+        }
+
+        public void Launch()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
